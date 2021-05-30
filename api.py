439e05b1a57c0b5b4eb61ae235f69c6070237dad
Febri <mohammadfebriyanto@pahamify.com>
2021-05-30 09:28:55 +0700

    add handler

--- api.py
+++ api.py
@@ -20,20 +20,23 @@ def api_id():
 
 
     if ('long' in request.args) & ('lat' in request.args):
-        long = float(request.args['long'])
-        lat = float(request.args['lat'])
-        pm10, so2, co, o3, no2 = req(long,lat)
-        results = int(predict(pm10, so2, co, o3, no2))
-        if results==0:
-            hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'BAIK', 'rekomendasi': 'Silahkan berolahraga'}
-        elif results==1:
-            hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'SEDANG', 'rekomendasi': 'Silahkan berolahraga'}
-        elif results==2:
-            hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'TIDAK SEHAT', 'rekomendasi': 'Jangan berolahraga'}
-        elif results==3:
-            hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'SANGAT TIDAK SEHAT', 'rekomendasi': 'Jangan berolahraga'}
-        else:
-            hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'BERBAHAYA', 'rekomendasi': 'Lebih baik di rumah'}
+        try:
+            long = float(request.args['long'])
+            lat = float(request.args['lat'])
+            pm10, so2, co, o3, no2 = req(long,lat)
+            results = int(predict(pm10, so2, co, o3, no2))
+            if results==0:
+                hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'BAIK', 'rekomendasi': 'Silahkan berolahraga'}
+            elif results==1:
+                hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'SEDANG', 'rekomendasi': 'Silahkan berolahraga'}
+            elif results==2:
+                hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'TIDAK SEHAT', 'rekomendasi': 'Jangan berolahraga'}
+            elif results==3:
+                hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'SANGAT TIDAK SEHAT', 'rekomendasi': 'Jangan berolahraga'}
+            else:
+                hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'BERBAHAYA', 'rekomendasi': 'Lebih baik di rumah'}
+        except:
+            return "Error: longitude and latitude is not valid"
     else:
         return "Error: No location field provided. Please specify longitude and latitude."
     return jsonify(hasil)
