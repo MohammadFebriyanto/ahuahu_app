bae1da265b045ee5690998a40fcb59f992ee88ff
Febri <mohammadfebriyanto@pahamify.com>
2021-06-04 23:24:21 +0700

    update api

--- api/api.py
+++ api/api.py
@@ -26,15 +26,15 @@ def api_id():
             pm10, so2, co, o3, no2 = req(long,lat)
             results = int(predict(pm10, so2, co, o3, no2))
             if results==0:
-                hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'Good', 'rekomendasi': 'Let\'s do some exercise'}
+                hasil = {'link': 'https://storage.googleapis.com/bucket_ahuahu/smile_green.png', 'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'Good', 'rekomendasi': 'Let\'s do some exercise', 'deskripsi': 'this location has good air level, level of carbon monoxide is {}, Nitrogen dioxide is {}, Ozone is {}, particullate matter 10 is {}, Sulfur dioxide is {}, you can exercise with a fresh air, enjoy to exercise around here. we recommend you to exercise outdoor to feel a good air around you. you can do cycling, running, or jogging. Have a nice day'.format(co, no2, o3, pm10, so2)}
             elif results==1:
-                hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'Moderate', 'rekomendasi': 'Prefer you stay at home'}
+                hasil = {'link': 'https://storage.googleapis.com/bucket_ahuahu/smile_grey.png', 'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'Moderate', 'rekomendasi': 'Prefer you stay at home','deskripsi':'this location has moderate air level, level of carbon monoxide is {}, Nitrogen dioxide is {}, Ozone is {}, particullate matter 10 is {}, Sulfur dioxide is {}. we do not recommend to exercise in this area. we recommend to exercise indoor than you exercise outdoor. you can exercise together with your friends and family. Have a nice day and stay healthy '.format(co, no2, o3, pm10, so2)}
             elif results==2:
-                hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'Unhealthy', 'rekomendasi': 'Prefer you stay at home'}
+                hasil = {'link': 'https://storage.googleapis.com/bucket_ahuahu/smile_grey.png', 'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'Unhealthy', 'rekomendasi': 'Prefer you stay at home','deskripsi':'this location has unhealthy air level, level of carbon monoxide is {}, Nitrogen dioxide is {}, Ozone is {}, particullate matter 10 is {}, Sulfur dioxide is {}, we do not recommend to exercise in this area. we recommend to exercise indoor than you exercise outdoor. you can exercise together with your friends and family. Have a nice day and stay healthy'.format(co, no2, o3, pm10, so2)}
             elif results==3:
-                hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'Very Unhealthy', 'rekomendasi': 'Avoid outdoor activity'}
+                hasil = {'link': 'https://storage.googleapis.com/bucket_ahuahu/smile_red.png', 'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'Very Unhealthy', 'rekomendasi': 'Avoid outdoor activity', 'deskripsi': 'this location has very unhealthy air level, level of carbon monoxide is {}, Nitrogen dioxide is {}, Ozone is {}, particullate matter 10 is {}, Sulfur dioxide is {}, we do not recommend to exercise in this area. we recommend to exercise indoor than you exercise outdoor. you can exercise together with your friends and family. Have a nice day and stay healthy'.format(co, no2, o3, pm10, so2)}
             else:
-                hasil = {'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'Hazardous', 'rekomendasi': 'Please avoid outdoor activity'}
+                hasil = {'link': 'https://storage.googleapis.com/bucket_ahuahu/smile_red.png', 'pm10':pm10, 'so2':so2, 'co':co, 'o3':o3, 'no2':no2, 'kualitas_udara': 'Hazardous', 'rekomendasi': 'Please avoid outdoor activity','deskripsi': 'this location has dangerous air level, level of carbon monoxide is {}, Nitrogen dioxide is {}, Ozone is {}, particullate matter 10 is {}, Sulfur dioxide is {}, we do not recommend to exercise in this area. we recommend to exercise indoor than you exercise outdoor. you can exercise together with your friends and family. Have a nice day and stay healthy'.format(co, no2, o3, pm10, so2)}
         except:
             return "Error: longitude and latitude is not valid"
     else:
@@ -42,4 +42,4 @@ def api_id():
     return jsonify(hasil)
 
 if __name__ == "__main__":
-    app.run(debug = True, host='0.0.0.0', port='80')
+    app.run(debug = True, host='0.0.0.0', port='7777')
